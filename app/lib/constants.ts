de094b6761290e96bb3647ad54b26f45529e65d4
Kemar James <kkj7854@rit.edu>
2025-07-18 17:04:58 -0400

    update envs

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const TMDB_WEBSITE_URL = import.meta.env.VITE_TMDB_WEBSITE_URL;
+export const API_BASE_URL = process.env.API_BASE_URL;
+export const TMDB_WEBSITE_URL = process.env.TMDB_WEBSITE_URL;
